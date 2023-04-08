50af321f991982c19070460cce40774223518ffd
912-Dragan-Andrei <91147022+912-Dragan-Andrei@users.noreply.github.com>
2023-04-08 15:30:33 +0300

    [29] fixed frontend redirect

--- cafe/client/src/utils/constants.ts
+++ cafe/client/src/utils/constants.ts
@@ -1,4 +1,4 @@
-const PROD_BACKEND_API_URL = "/api";
+const PROD_BACKEND_API_URL = "https://eveiller.ignorelist.com/api";
 const DEV_BACKEND_API_URL = "http://127.0.0.1:8000/api";
 
 export const BASE_URL_API =
