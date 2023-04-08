48d62c07cd8eabc45d29e65cde0c88b264ec28fa
912-Dragan-Andrei <91147022+912-Dragan-Andrei@users.noreply.github.com>
2023-04-08 17:21:54 +0300

    [30] - done

--- cafe/server/blends_api/models.py
+++ cafe/server/blends_api/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 
+
 def validate_blend_level(value):
     if value < 1 or value > 5:
-        raise ValidationError('strength level should be between 1 and 5', params={'value': value})
+        raise ValidationError('strength level should be between 1 and 5',
+                              params={'value': value})
+
 
 # Create your models here.
 class Blend(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=1000)
     country_of_origin = models.CharField(max_length=50)
     level = models.IntegerField(validators=[validate_blend_level])
