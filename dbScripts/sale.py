40f71a2b7c23e91f6ff6327e774fe9e200045c0a
912-Dragan-Andrei <91147022+912-Dragan-Andrei@users.noreply.github.com>
2023-04-09 19:56:31 +0300

    [35]

--- dbScripts/sale.py
+++ dbScripts/sale.py
@@ -20,15 +20,15 @@ with open('populateSales.sql', 'w') as f:
         file=f)
 
     # generate new records to insert
-    for i in range(1000):
+    for i in range(10000):
         if (i % 100 == 0):
-            print(f'Generated {i * 10000} records')
+            print(f'Generated {i * 1000} records')
 
         # generate a fake coffee_id
-        temp_coffee_id = fake.random_int(min=i * 1000 + 1, max=(i + 1) * 1000)
+        temp_coffee_id = fake.random_int(min=i * 100 + 1, max=(i + 1) * 100)
 
         values = []
-        for j in range(10000):
+        for j in range(1000):
             # generate a fake sold_coffees that is between 1 and 1000
             sold_coffees = fake.random_int(min=1, max=1000)
 
@@ -39,8 +39,8 @@ with open('populateSales.sql', 'w') as f:
                                    max_value=1000)
 
             # generate a fake location_id
-            temp_location_id = fake.random_int(min=j * 100 + 1,
-                                               max=(j + 1) * 100)
+            temp_location_id = fake.random_int(min=j * 1000 + 1,
+                                               max=(j + 1) * 1000)
 
             values.append(
                 f'({sold_coffees}, {revenue}, (SELECT coffee_id FROM temp_coffee_ids WHERE temp_id = {temp_coffee_id}), (SELECT location_id FROM temp_location_ids WHERE temp_id = {temp_location_id}))'
