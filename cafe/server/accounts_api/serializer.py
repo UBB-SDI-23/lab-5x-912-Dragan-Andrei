c1e8c804ab29fa6f8356a4be11c00c94c7d897da
912-Dragan-Andrei <91147022+912-Dragan-Andrei@users.noreply.github.com>
2023-05-02 23:06:07 +0300

    [49] - done (#3)

---
+++ cafe/server/accounts_api/serializer.py
@@ -0,0 +1,15 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        token['is_superuser'] = user.is_superuser
+        token['is_staff'] = user.is_staff
+        token['is_active'] = user.is_active
+        return token
--- cafe/server/accounts_api/urls.py
+++ cafe/server/accounts_api/urls.py
@@ -1,16 +1,15 @@
 from django.urls import path
 
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+    TokenRefreshView, )
 
 from .views.register_view import Register
 from .views.confirmation_view import ConfirmAccount
+from .views.token_view import MyTokenObtainPairView
 
 urlpatterns = [
     path('register/', Register.as_view()),
     path('register/confirm/', ConfirmAccount.as_view()),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
