a9d5a3828650aa6d75bc198f8d5703b2676860df
912-Dragan-Andrei <91147022+912-Dragan-Andrei@users.noreply.github.com>
2023-05-12 10:24:38 +0300

    nginx config

--- cafe/server/cafe/settings.py
+++ cafe/server/cafe/settings.py
@@ -11,14 +11,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('secret')
 DEBUG = True
 ALLOWED_HOSTS = ['*']
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
